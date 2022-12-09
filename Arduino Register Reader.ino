8b92eae24bc3077997dab1bec15eaa99c92ede2a
kaio37k <bradpearson37@gmail.com>
2022-12-08 21:53:29 -0800

    Create Arduino Register Reader.ino

---
+++ Arduino Register Reader.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+
+void setup() {
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+
+  delay(100);
+
+  Serial.println("Setup Started");
+  // Wire.write(0x07); // DONE
+
+}
+
+void loop() {
+  Wire.beginTransmission(0x10); // START
+  Wire.write(0x4C);
+  Wire.endTransmission(false);
+  
+  Wire.requestFrom(0x10, 1);
+  byte receivedByte = Wire.read();
+  Serial.println(receivedByte);
+  
+  // delay(500);
+}
