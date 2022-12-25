8c30444ba5a76084df1fec9835238b0ef5130313
kaio37k <bradpearson37@gmail.com>
2022-12-24 23:39:26 -0800

    Create Main.c
    Was necessary to add DELAY function, since it was causing issues NOT having it.

---
+++ Transmitter/2 - Functional/Reading Sensor Registers V0.9/Main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "boards.h"
+#include "app_util_platform.h"
+#include "app_error.h"
+#include "nrf_drv_twi.h"
+#include "nrfx_twi.h"
+#include "nrf_delay.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+#include "bmm150.h"
+#include "bmm150_common.h"
+
+//=====================================================
+//                        MAIN
+//=====================================================
+
+int main(void)
+{
+    struct bmm150_settings mySettings;
+    struct bmm150_raw_mag_data raw_mag;
+    struct bmm150_mag_data mag_data;
+    struct bmm150_dev dev;
+
+    dev.intf = BMM150_I2C_INTF;
+    //dev.intf_ptr = BMM150_DEFAULT_I2C_ADDRESS;
+    dev.delay_us = bmm150_user_delay_us;
+    dev.read = bmm150_user_i2c_reg_read;
+    dev.write = bmm150_user_i2c_reg_write;
+    dev.pwr_cntrl_bit = 1;
+
+    bmm150_interface_selection(&dev);
+
+    uint8_t asd[] = {9};
+    uint8_t dsa[] = {8};
+    
+    //printf("---------------------------\n");
+
+    //bmm150_user_i2c_reg_read(0x4b, dsa, 1, 0x10);
+
+    //bmm150_get_regs(0x40, dsa, 1, &dev);
+
+    //printf("---------------------------\n");
+    //printf("dsa:\t%x\n", dsa[0]);
+
+    bmm150_init(&dev);
+
+    //writeOneRegister(0x4b, 0x01);
+
+    //mySettings.pwr_mode = BMM150_POWERMODE_NORMAL;
+    //bmm150_set_op_mode(&mySettings, &dev);
+
+    //mySettings.preset_mode = BMM150_PRESETMODE_REGULAR;
+    //bmm150_set_presetmode(&mySettings, &dev);
+
+    //writeOneRegister(0x4b, 0x82);     //  SOFT  RESET
+    //writeOneRegister(0x4b, 0x01);     //  POWER CONTROL BIT,  ON
+    //writeOneRegister(0x4C, 0x00);     //  SET   OP  MODE
+
+}
